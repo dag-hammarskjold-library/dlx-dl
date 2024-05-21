68484de1a8b02338e7afc3ba462c6a0f4f981d5a
jbukhari <joe.bukhari@gmail.com>
2024-05-21 09:41:05 -0400

    fix field deletion

--- dlx_dl/scripts/sync/__init__.py
+++ dlx_dl/scripts/sync/__init__.py
@@ -496,10 +496,13 @@ def compare_and_update(args, *, dlx_record, dl_record):
                 continue
 
         if normalize(field.to_mrk()) not in [normalize(x) for x in dlx_fields_serialized]:
-            if field.tag in [x.tag for x in dlx_fields]:
+            # compare tag + indicators
+            if field.tag + ''.join(field.indicators) in [x.tag + ''.join(x.indicators) for x in dlx_fields]:
+                # this should already be taken care of in dlx->dl
                 print(f'{dlx_record.id}: SUPERCEDED: {field.to_mrk()}')
                 take_tags.add(field.tag)
             else:
+                # delete fields where the tag + indicators combo does not exist in dl record
                 print(f'{dlx_record.id}: TO DELETE: {field.to_mrk()}')
 
                 # use the field in the export to delete the field in DL by setting values to empty string
