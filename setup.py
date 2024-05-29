e5ed791889163789072f00ef286f442933461ee7
Aaron Helton <helton@un.org>
2024-05-29 14:55:00 -0400

    and bump version in setup.py to 1.2.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt") as f:
 
 setup(
     name = 'dlx_dl',
-    version = '1.2.3',
+    version = '1.2.4',
     url = 'http://github.com/dag-hammarskjold-library/dlx-dl',
     author = 'United Nations Dag Hammarskjöld Library',
     author_email = 'library-ny@un.org',
