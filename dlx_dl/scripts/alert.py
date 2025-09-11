6db81bbfecae63fb127565bbbbffa2e4ec870c89
jbukhari <joe.bukhari@gmail.com>
2025-09-11 17:30:32 -0400

    Alert refactor (#221)
    * update alert logic
    
    * pass tests
    
    * refactor
    
    * implement alert frequency
    
    * bugfix
    
    ---------
    
    Co-authored-by: jbukhari <jbukhari@gmail.com>

--- dlx_dl/scripts/alert.py
+++ dlx_dl/scripts/alert.py
@@ -1,69 +1,94 @@
-import sys, os
+import sys, os, re
 from argparse import ArgumentParser
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta, timezone
 import boto3
 from dlx import DB
-from dlx.marc import Bib, Auth
+from dlx.marc import Marc, Bib, Auth
+from dlx_dl.util import elapsed, PendingStatus
 
 ap = ArgumentParser()
-ap.add_argument('--connect')
-ap.add_argument('--database')
+ap.add_argument('--connect', required=True)
+ap.add_argument('--database', required=True)
+ap.add_argument('--pending_time', required=True, type=int, help='Send alert if records have been pending more than this number of seconds')
+ap.add_argument('--alert_frequency', type=int, default=21600, help='Skip alert if last alert was within this number of seconds')
+mg = ap.add_mutually_exclusive_group(required=True)
+mg.add_argument('--topic_arn', help='AWS SNS topic ARN')
+mg.add_argument('--phone_number', help='AWS SNS topic phone number')
 
-def run() -> bool:
+def run() -> dict:
     args = ap.parse_args()
     DB.connect(args.connect, database=args.database) if DB.connected is False else None # if testing, already connected to DB
-    log = DB.handle.get_collection('dlx_dl_log')
-    alert = False
+    statuses = []
 
+    # Check bibs and auths for export pending time
     for cls in (Bib, Auth):
-        last_updated_record = cls.from_query({}, sort=[('updated', -1)], limit=1)
-
-        if elapsed(last_updated_record.updated).seconds < 7200:
-            
-            # Records have been updated in the last two hours
-            last_exported = log.find_one({'source': 'dlx-dl-lambda', 'record_type': 'bib' if cls == Bib else 'auth'}, sort=[('time', -1)])
-
-            if elapsed(last_exported['time']).seconds > 7200:
-                # It's been more than two hours since records were exported to UNDL
-                alert = True
-                message = f"It's been more than two hours between {'bib' if cls == Bib else 'auth'} updates in Central DB and exports to UNDL"
-                print('Sending alert notification that records are out of sync')
-                notify(message)
-
-    return alert
-                
-def elapsed(since: datetime, until: datetime = datetime.now(timezone.utc)) -> timedelta:
-    """Returns the time elapsed between two datetimes as a timedelta"""
-
-    # Timezones have to be set to subtract datetimes. Assume both utc
-    until, since = [x.replace(tzinfo=timezone.utc) for x in (until, since)]
-
-    return until - since
-
-def notify(message: str) -> bool:
-    client = boto3.client('sns')
-   
-    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sns/client/publish.html
-    response = client.publish(
-        Message=message,
-        # need a Topic, Target or Phone Number set up in AWS SNS
-        #TopicArn='x:x:x:x:x' if DB.database_name == 'testing' else '?', # can't use fake TopicArn in tests?
-        #TargetArn='string',
-        PhoneNumber='5555555555' if DB.database_name == 'testing' else None, 
+        last_updated = cls.from_query({}, sort=[('updated', -1)], limit=1)
         
-        # below not required
-        #Subject='string',
-        #MessageStructure='string',
-        #MessageAttributes={
-        #    'string': {
-        #        'DataType': 'string',
-        #        'StringValue': 'string',
-        #        'BinaryValue': b'bytes'
-        #    }
-        #},
-        #MessageDeduplicationId='string',
-        #MessageGroupId='string'
-    )
+        # It's been more than two hours since last record updated, indicating system inactivity
+        if elapsed(last_updated.updated.replace(tzinfo=timezone.utc)).seconds > 7200:
+            continue
+        
+        statuses.append(PendingStatus(collection='bibs' if cls == Bib else 'auths'))
+    
+    if statuses := [x for x in statuses if x.pending_time > args.pending_time]:
+        # At least one collection has exports pending longer than the max alert time
+        alert_collection = DB.handle.get_collection('dlx_dl_alert')
+        
+        # Abort if alerts have been sent within the alert frequency
+        if len(statuses) == 2:
+            last_bib_alert = alert_collection.find_one({'collection': 'bibs'}, sort=[('time', -1)]) or {'time': datetime.min}
+            last_auth_alert = alert_collection.find_one({'collection': 'auths'}, sort=[('time', -1)]) or {'time': datetime.min}
+
+            if all([elapsed((x.get('time')).replace(tzinfo=timezone.utc)).total_seconds() < args.alert_frequency for x in (last_bib_alert, last_auth_alert)]):
+                return
+        elif last_alert := alert_collection.find_one({'collection': statuses[0].collection}):
+            if elapsed(last_alert.get('time').replace(tzinfo=timezone.utc)).total_seconds() < args.alert_frequency:
+                return
+
+        return notify(topic_arn=args.topic_arn, statuses=statuses)
+
+    return
+
+def notify(*, topic_arn: str, phone_number=None, statuses: list[PendingStatus] = []) -> dict:
+    if not statuses:
+        return 
+    
+    client = boto3.client('sns')
+    message = ''
+
+    for status in statuses:
+        minutes = int(status.pending_time / 60)
+        message = message if message else 'Hello,'
+        message += f'\n\n{"Bib" if status.collection == "bibs" else "Auth"} exports have been pending for more than {minutes} minutes.'
+    
+    if message:
+        subject = f'UNDL exports pending: exports to UNDL have been pending for more than {minutes} minutes'
+        message += '\n\nSee https://cloudwatch.amazonaws.com/dashboard.html?dashboard=DLX-DL&context=eyJSIjoidXMtZWFzdC0xIiwiRCI6ImN3LWRiLTk1MDIzNjUzNzk0OSIsIlUiOiJ1cy1lYXN0LTFfb256a1pUMmphIiwiQyI6IjZycXFqbm1mZzc2c3RyZzc3ZTZiZW1pbmZzIiwiSSI6InVzLWVhc3QtMTpmMjZiMzkxOC1lNDBjLTQwNzktODIyMy0zZWEzMDViZjA2N2IiLCJPIjoiYXJuOmF3czppYW06Ojk1MDIzNjUzNzk0OTpyb2xlL3NlcnZpY2Utcm9sZS9DV0RCU2hhcmluZy1QdWJsaWNSZWFkT25seUFjY2Vzcy1LNEFMMExLUyIsIk0iOiJQdWJsaWMifQ%3D%3D&start=PT12H&end=null#dashboards:'
+    
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sns/client/publish.html
+    # Can use a fake phone number in mock aws, but not fake arn
+    kwargs = {'PhoneNumber': '+15555555555'} if DB.database_name == 'testing' else phone_number or {'TopicArn': topic_arn}
+    kwargs.update({'Subject': subject, 'Message': message})
+
+    if result := client.publish(**kwargs):
+        for status in statuses:
+            print('update db')
+
+            DB.handle.get_collection('dlx_dl_alert').insert_one(
+                {
+                    'time': datetime.now(timezone.utc),
+                    'collection': status.collection,
+                    'pending_time': status.pending_time
+                }
+            )
+
+        return result
+
+###
 
 if __name__ == '__main__':
+    # The file is being run by the Python interpreter as a script
     run()
+elif not sys.argv[1:]:
+    # The function is being imported from another script or module
+    raise Exception('Arguments must be provided through sys.argv')
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,8 +1,6 @@
-import sys, pytest, time
-import boto3
-from moto import mock_aws
+import pytest, math
 from datetime import datetime, timezone, timedelta
-from dlx.marc import DB, Bib, Auth
+from dlx import DB
 
 @pytest.fixture
 def db():
@@ -32,45 +30,21 @@ def db():
 
     return DB.client
 
-@mock_aws
-def test_run(db):
-    sys.argv[1:] = [
-        '--connect', 'mongomock://localhost', 
-        '--database', 'testing', 
-        '--topic_arn', 'x:x', 
-        '--pending_time', '7200',
-        '--alert_frequency', '21600'
-    ]
-    from dlx_dl.scripts import alert
+def test_pending_status(db):
+    from dlx_dl.util import PendingStatus
 
-    # Records have been pending for more than two hours
-    assert alert.run()
+    status = PendingStatus(collection='bibs')
+    assert round(status.pending_time / 60 / 60) == 3 # round to 3 hours
+    assert len(status.pending_records) == 2
 
-    # Alerts have been sent within alert frequency
-    assert alert.run() is None
-
-    # Delete the alert log info
-    DB.handle.get_collection('dlx_dl_alert').delete_many({})
-
-    # A bib has been exported within two hours, but not auth
     DB.handle['dlx_dl_log'].insert_one(
         {
             'record_type': 'bib',
             'time': datetime.now(timezone.utc) - timedelta(hours=1),
             'source': 'dlx-dl-lambda'
-        },
+        }
     )
-    assert alert.run()
 
-    # Auths and bibs have been pending less than two hours. No alert
-    DB.handle['dlx_dl_log'].insert_one(
-        {
-            'record_type': 'auth',
-            'time': datetime.now(timezone.utc) - timedelta(hours=1),
-            'source': 'dlx-dl-lambda'
-        },
-    )
-    assert alert.run() is None
-
-
-
+    status = PendingStatus(collection='bibs')
+    assert status.pending_time == 0
+    assert len(status.pending_records) == 0
