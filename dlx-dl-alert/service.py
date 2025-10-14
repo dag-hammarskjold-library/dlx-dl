6aab22db36b3685df6a690dfeda00f1f9c8e449f
jbukhari <joe.bukhari@gmail.com>
2025-10-14 16:46:05 -0400

    Create and deploy the alert Lambda function (#233)
    * use ssm; update messaging; rename dirs
    
    * rename lambda function dirs
    
    * get topic arn from ssm
    
    * add alert lambda function and deployment workflow
    
    * Adjust workflow to use a different ECR repository (#238)
    
    * add workflow dispatch to run workflow manually and add secret to construct ARN

---
+++ dlx-dl-alert/service.py
@@ -0,0 +1,12 @@
+import sys
+from dlx_dl.scripts import alert
+
+def handler():
+    # Different approach here. Feed the arguments through sys.argv
+    sys.argv[:1] = ['pending_time=7200', 'alert_frequency=21600'] 
+
+    try:
+        alert.run()
+    except Exception as exc:
+        print('; '.join(str(exc).split('\n'))) # puts exception text on one line for CloudWatch logs
+
