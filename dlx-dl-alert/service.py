711a6165c0384b97e683a6f87cb0de416cf0b384
jbukhari <joe.bukhari@gmail.com>
2025-10-20 11:41:18 -0400

    Fix args

--- dlx-dl-alert/service.py
+++ dlx-dl-alert/service.py
@@ -3,7 +3,7 @@ from dlx_dl.scripts import alert
 
 def handler(event, context):
     # Different approach here. Feed the arguments through sys.argv
-    sys.argv[:1] = ['pending_time=7200', 'alert_frequency=21600'] 
+    sys.argv[:1] = ['--pending_time=7200', '--alert_frequency=21600'] 
 
     try:
         alert.run()
