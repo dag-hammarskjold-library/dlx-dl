0fb8cafb9491b7f98c37c323f31efd304cfec2d3
jbukhari <jbukhari@gmail.com>
2025-10-20 12:10:45 -0400

    more arg fix

--- dlx-dl-alert/service.py
+++ dlx-dl-alert/service.py
@@ -3,10 +3,9 @@ from dlx_dl.scripts import alert
 
 def handler(event, context):
     # Different approach here. Feed the arguments through sys.argv
-    sys.argv[:1] = ['--pending_time=7200', '--alert_frequency=21600'] 
+    sys.argv[1:] = ['--pending_time=7200', '--alert_frequency=21600'] 
 
     try:
         alert.run()
     except Exception as exc:
         print('; '.join(str(exc).split('\n'))) # puts exception text on one line for CloudWatch logs
-
