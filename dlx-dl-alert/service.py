cea93257c6b1c5b61baf7b5fd254491da71b3802
jbukhari <joe.bukhari@gmail.com>
2025-10-20 11:24:32 -0400

    Alert handler params fix (#249)

--- dlx-dl-alert/service.py
+++ dlx-dl-alert/service.py
@@ -1,7 +1,7 @@
 import sys
 from dlx_dl.scripts import alert
 
-def handler():
+def handler(event, context):
     # Different approach here. Feed the arguments through sys.argv
     sys.argv[:1] = ['pending_time=7200', 'alert_frequency=21600'] 
 
